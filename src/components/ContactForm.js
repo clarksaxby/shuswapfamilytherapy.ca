3d65e192eb7b72bd91d6609cd6031641565cb889
Monika Piechatzek <piechatzek@hotmail.com>
2018-07-09 16:25:26 -0700

    Added basic component setup for contact form

---
+++ src/components/ContactForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+// import styled from 'styled-components'
+
+class ContactForm extends React.Component {
+  render() {
+    return <div>sup</div>
+  }
+}
+
+export default ContactForm
