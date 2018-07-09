1080ef1685cc80791ecd0ea74acb29501258d3b0
Monika Piechatzek <piechatzek@hotmail.com>
2018-07-09 11:55:10 -0700

    Added all icons needed

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -2,12 +2,35 @@ import React from 'react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import faFacebookF from '@fortawesome/fontawesome-free-brands/faFacebookF'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 
-const IconsStyle = styled(FontAwesomeIcon)``
+const IconsStyle = styled.div`
+  border: 1px solid;
+  border-radius: 50%;
+  padding: 17px 17px;
+  color: white;
+  opacity: 0.5;
+
+  &: hover {
+    opacity: 1;
+  }
+`
+const Icon = styled(FontAwesomeIcon)``
+// width: 50px;
+// height: 50px;
+
 const Icons = props => {
   return (
     <div {...props}>
-      <IconsStyle icon={faFacebookF} />
+      <IconsStyle>
+        <Icon icon={faFacebookF} />
+      </IconsStyle>
+      <IconsStyle>
+        <Icon icon={faEnvelope} />
+      </IconsStyle>
+      <IconsStyle>
+        <Icon icon={faPhone} />
+      </IconsStyle>
     </div>
   )
 }
