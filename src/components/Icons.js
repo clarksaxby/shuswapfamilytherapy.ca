707d176c4fd349bf9c98b439f350d02f055efd58
Monika Piechatzek <piechatzek@hotmail.com>
2018-07-09 14:23:13 -0700

    Added css grid for aligning icons

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,29 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import faFacebookF from '@fortawesome/fontawesome-free-brands/faFacebookF'
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 
+const IconsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 17px;
+`
 const IconsStyle = styled.div`
   border: 1px solid;
   border-radius: 50%;
-  padding: 17px 17px;
-  border-color: rgba(255, 255, 255, 0.5);
-
+  padding: 18px;
+  border-color: rgba(255, 255, 255, 0.35);
+  transition: background-color 0.2s ease-in-out, border-color 0.2s ease-in-out;
+  text-align: center;
   &: hover {
-    opacity: 1;
-    background-color: rgba(255, 255, 255, 0.5);
+    transition: background-color 0.2s ease-in-out, border-color 0.2s ease-in-out;
+    background-color: rgba(255, 255, 255, 0.35);
+    border-color: rgba(255, 255, 255, 1);
   }
 `
 const Icon = styled(FontAwesomeIcon)`
   color: white;
+  font-size: 2em;
 `
-// width: 50px;
-// height: 50px;
 
 const Icons = props => {
   return (
-    <div {...props}>
+    <IconsWrapper {...props}>
       <IconsStyle>
         <Icon icon={faFacebookF} />
       </IconsStyle>
@@ -33,7 +39,7 @@ const Icons = props => {
       <IconsStyle>
         <Icon icon={faPhone} />
       </IconsStyle>
-    </div>
+    </IconsWrapper>
   )
 }
 
