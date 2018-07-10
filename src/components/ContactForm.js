6af81a3e9c91985c5d60c579acbde214c5bd63aa
Monika Piechatzek <piechatzek@hotmail.com>
2018-07-10 10:48:05 -0700

    Added Header and Button to form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,10 +1,19 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 // import styled from 'styled-components'
+import Header from './Header'
+import Button from './Button'
 
 class ContactForm extends React.Component {
   render() {
-    return <div>sup</div>
+    return (
+      <div>
+        <Header title="Make an Appointment">
+          #3-781 Marine Park Drive Salmon Arm, BC V1E2W7
+        </Header>
+        <Button color="white">Send</Button>
+      </div>
+    )
   }
 }
 
