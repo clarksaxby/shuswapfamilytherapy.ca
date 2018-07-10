c55930eebf57ce4a72ec7fe1a939b93fbe00f661
Monika Piechatzek <piechatzek@hotmail.com>
2018-07-10 10:49:27 -0700

    Added background decorator to contact form story

--- src/stories/ContactForm.js
+++ src/stories/ContactForm.js
@@ -5,5 +5,5 @@ import { host } from 'storybook-host'
 import ContactForm from '../components/ContactForm'
 
 storiesOf('Contact Form', module)
-  .addDecorator(host())
+  .addDecorator(host({ background: '#9088D9' }))
   .add('Default empty form', () => <ContactForm />)
