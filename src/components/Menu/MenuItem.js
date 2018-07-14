c574d2a79c03a8f41338a2649640c9d4532b63e9
Caleb Sharp <epicsharp11@gmail.com>
2018-07-14 10:31:41 -0700

    Update MenuItem to use activeClassName and gatsby-link

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -1,58 +1,55 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import Link from 'gatsby-link'
 
-export const LinkListItem = styled.li`
+const activeClassName = 'activeMenuLink'
+export const MenuLink = styled(Link).attrs({
+  activeClassName,
+})`
   position: relative;
   display: inline-block;
   margin: 0 0.75rem;
   padding: 0;
+  text-decoration: none;
   cursor: pointer;
 
-  ${({ current }) =>
-    current &&
-    css`
-      &:after {
-        content: '';
-        display: block;
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 3px;
-        background-image: linear-gradient(90deg, #a2cedc, #aa8fff);
-      }
-    `};
+  &.${activeClassName} {
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 3px;
+      background-image: linear-gradient(90deg, #a2cedc, #aa8fff);
+    }
+  }
 `
 
-export const MenuLink = styled(Link)`
+export const MenuLinkContent = styled.span`
   display: block;
   height: inherit;
   line-height: inherit;
   border-bottom: 0;
   padding: 0 0.75rem;
   font-weight: 600;
-  text-decoration: none;
   color: #7a7d86;
 
   &:hover {
     color: #a3a9e7;
   }
 
-  ${({ current }) =>
-    current &&
-    css`
-      color: #a3a9e7;
-    `};
+  .${activeClassName} > & {
+    color: #a3a9e7;
+  }
 `
 
-const MenuItem = ({ current, children, ...props }) => (
-  <LinkListItem current={current}>
-    <MenuLink current={current} {...props}>
-      {children}
-    </MenuLink>
-  </LinkListItem>
+const MenuItem = ({ children, ...props }) => (
+  <MenuLink activeClassName={activeClassName} {...props}>
+    <MenuLinkContent>{children}</MenuLinkContent>
+  </MenuLink>
 )
 
 MenuItem.propTypes = {
