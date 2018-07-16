47ad50e54bfe115248c59243a8e2915b04079ac7
Caleb Sharp <epicsharp11@gmail.com>
2018-07-16 13:15:08 -0700

    Add subtitle to Header

--- src/stories/HeroBanner.js
+++ src/stories/HeroBanner.js
@@ -16,3 +16,15 @@ storiesOf('HeroBanner', module)
       onAction={testAction()}
     />
   ))
+  .add('With subtitle', () => (
+    <HeroBanner title="Lorem Ipsum" subtitle="Dolor sit amet" />
+  ))
+  .add('With everything', () => (
+    <HeroBanner
+      image="http://placekitten.com/600/300"
+      title="Lorem Ipsum Dolor Sit Amet"
+      subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit tristique nisl, eu porttitor velit. Ut justo lectus, consequat at mauris quis, finibus venenatis lacus. Nam non metus laoreet, convallis turpis id, congue arcu. Phasellus lorem urna, rutrum vel libero vel, auctor condimentum nunc. Ut posuere enim non cursus lacinia."
+      action="Action"
+      onAction={testAction()}
+    />
+  ))
