66a5e8b7a112b887d81133cb61b98b75487067b5
Caleb Sharp <epicsharp11@gmail.com>
2018-07-16 11:29:28 -0700

    Remove duplicate overlay.png

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
 import Button from './Button'
-import overlay from './overlay.png'
+import overlay from '../layouts/images/overlay.png'
 
 const HeroContainer = styled.div`
   color: #ffffff;
