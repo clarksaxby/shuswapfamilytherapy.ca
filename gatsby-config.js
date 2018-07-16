24d0f3c505fa8d7f453930c203f3b7c637474119
Caleb Sharp <epicsharp11@gmail.com>
2018-07-16 11:26:29 -0700

    Change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Shuswap Family Therapy',
   },
   plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-eslint'],
 }
