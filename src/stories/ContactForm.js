7119b1a541ff61df9d005269c9b2da9062c092a8
Monika Piechatzek <piechatzek@hotmail.com>
2018-07-18 08:29:57 -0700

    Removed functionality in the stories

--- src/stories/ContactForm.js
+++ src/stories/ContactForm.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { host } from 'storybook-host'
-import queryString from 'query-string'
 import ContactForm from '../components/ContactForm'
-
-const redirect = queryString.parseUrl(location.href)
-//  Regardless of whether there is already search params in the url, this will ass thanks=true
-redirect.query.thanks = true
-//  This reconstructs the whole url to use for the redirect field
-const redirectUrl = `${redirect.url}?${queryString.stringify(redirect.query)}`
-const checkThanks = window.location.search.includes('thanks=true')
 
 storiesOf('Contact Form', module)
   .addDecorator(host({ background: '#90B6DF' }))
@@ -19,16 +11,6 @@ storiesOf('Contact Form', module)
         'https://mailhound.twostoryrobot.com?key=' +
         process.env.STORYBOOK_MAILHOUND_KEY
       }
-    />
-  ))
-  .add('empty form with working thankyou message', () => (
-    <ContactForm
-      action={
-        'https://mailhound.twostoryrobot.com?key=' +
-        process.env.STORYBOOK_MAILHOUND_KEY
-      }
-      showThanks={checkThanks}
-      redirectUrl={redirectUrl}
     />
   ))
   .add('form with thankyou message', () => (
