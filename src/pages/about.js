69c7830edaf07fcb584c8ed16553ef7be5b8e563
Monika Piechatzek <piechatzek@hotmail.com>
2018-07-18 08:18:50 -0700

    Added Content page comp and grid styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,66 +1,97 @@
 import React from 'react'
 import styled from 'styled-components'
-import Header from '../components/Header'
 import Kendra from '../layouts/images/kendra.png'
+import ContentPage from '../components/ContentPage'
 
 const ContentWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 5fr repeat(5, 0.5fr);
-  grid-gap: 20px;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 0.35fr repeat(4, 0.7fr);
+  grid-gap: 10px;
 `
-const Image = styled.img``
+const Image = styled.img`
+  grid-column: 2 / span 1;
+  grid-row: 1 / span 3;
+`
+const Paragraph1 = styled.div`
+  grid-column: 1 / span 1;
+  grid-row: 1 / span 1;
+`
+
+const Paragraph2 = styled.div`
+  grid-column: 1 / span 1;
+  grid-row: 2 / span 1;
+`
+const Paragraph3 = styled.div`
+  grid-column: 1 / span 1;
+  grid-row: 3 / span 1;
+`
+const Paragraph4 = styled.div`
+  grid-column: 1 / span 1;
+  grid-row: 4 / span 1;
+`
+const Paragraph5 = styled.div`
+  grid-column: 1 / span 2;
+  grid-row: 5 / span 1;
+`
+// align-self: center;
 
 const About = () => (
   <div>
-    <Header title="Kendra Kieft">
-      MAMFT, RCC <br />
-    </Header>
-    <ContentWrapper>
-      <Image src={Kendra} alt="Kendra" />
-      <div>
-        My name is Kendra Kieft and I am a Registered Clinical Counsellor
-        (#13285) with a Masters degree in Marriage and Family Therapy. I
-        consider it a privilege to work with couples, children, youth, and
-        families as they navigate the many stages of life and work towards
-        positive growth.
-      </div>
+    <ContentPage title="Kendra Kieft" subtitle="MAMFT, RCC">
+      <ContentWrapper>
+        <Image src={Kendra} alt="Kendra" />
+        <Paragraph1>
+          My name is Kendra Kieft and I am a Registered Clinical Counsellor
+          (#13285) with a Masters degree in Marriage and Family Therapy. I
+          consider it a privilege to work with couples, children, youth, and
+          families as they navigate the many stages of life and work towards
+          positive growth.
+        </Paragraph1>
 
-      <div>
-        Growing up in a small town in Northern Alberta, I learned many life
-        lessons about family, community, and the resilience of the human spirit.
-        I moved to Abbotsford, BC to study counselling and caregiving and had
-        the amazing opportunity to work with adults with special needs. Through
-        my encounters with unique individuals I continued to learn about the
-        power of relationship and story. I believe we all desire to be accepted,
-        loved, and seen for who we really are. This desire for connection
-        propels us into relationships; a complicated combination of hurt and
-        healing. It is my desire to support individuals in creating healthy,
-        supportive relationships with self and others.
-      </div>
-      <div>
-        I have lived in the beautiful Shuswap area since 2008 with my husband
-        and three lively sons. I returned to school to begin my Masters degree
-        in 2012, and at the same time began working part-time as a school
-        counsellor at {`King's`} Christian School in Salmon Arm. I have also
-        been working at the Shuswap Family Resource & Referral Centre in Salmon
-        Arm since 2015. I am committed to utilizing best practices for therapy,
-        to engage in ongoing supervision and consultation, and to always be a
-        lifelong learner.
-      </div>
-      <div>
-        Common Issues I work with: Family & Couple Relationships Anxiety in
-        Adults and/or Children Depression in Adults and/or Children Self-harm
-        and Suicidal Ideation Self-Esteem and Self-Worth Stress Management Grief
-        and Loss
-      </div>
-      <div>
-        The clinical therapy I provide meets the high standards of the British
-        Columbia Association of Clinical Counsellors (BCACC.) Please visit the
-        {`BCACC's`} website for more information on Registered Clinical
-        Counsellors.
-      </div>
-    </ContentWrapper>
+        <Paragraph2>
+          Growing up in a small town in Northern Alberta, I learned many life
+          lessons about family, community, and the resilience of the human
+          spirit. I moved to Abbotsford, BC to study counselling and caregiving
+          and had the amazing opportunity to work with adults with special
+          needs. Through my encounters with unique individuals I continued to
+          learn about the power of relationship and story. I believe we all
+          desire to be accepted, loved, and seen for who we really are. This
+          desire for connection propels us into relationships; a complicated
+          combination of hurt and healing. It is my desire to support
+          individuals in creating healthy, supportive relationships with self
+          and others.
+        </Paragraph2>
+        <Paragraph3>
+          I have lived in the beautiful Shuswap area since 2008 with my husband
+          and three lively sons. I returned to school to begin my Masters degree
+          in 2012, and at the same time began working part-time as a school
+          counsellor at {`King's`} Christian School in Salmon Arm. I have also
+          been working at the Shuswap Family Resource & Referral Centre in
+          Salmon Arm since 2015. I am committed to utilizing best practices for
+          therapy, to engage in ongoing supervision and consultation, and to
+          always be a lifelong learner.
+        </Paragraph3>
+        <Paragraph4>
+          Common Issues I work with:
+          <ul>
+            <li>Family & Couple Relationships </li>
+            <li> Anxiety in Adults and/or Children</li>
+            <li>Depression in Adults and/or Children</li>
+            <li>Self-harm and Suicidal Ideation</li>
+            <li>Self-Esteem and Self-Worth</li>
+            <li>Stress Management</li>
+            <li>Grief and Loss</li>
+          </ul>
+        </Paragraph4>
+        <Paragraph5>
+          The clinical therapy I provide meets the high standards of the British
+          Columbia Association of Clinical Counsellors (BCACC.) Please visit the
+          {` BCACC's`} website for more information on Registered Clinical
+          Counsellors.
+        </Paragraph5>
+      </ContentWrapper>
+    </ContentPage>
   </div>
 )
 
