c34cf6fe5db2b842604e90b547721f35b5c47716
Caleb Sharp <epicsharp11@gmail.com>
2018-07-18 10:13:37 -0700

    Fix background colour of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = styled.footer`
   width: 100%;
   height: 13rem;
   padding: 2rem 0;
-  background-image: url('${overlay}'), linear-gradient(45deg, #8e94f8, #bc9cba);
+  background-image: url('${overlay}'), linear-gradient(45deg, #84c8da, #88a8ec);
   color: #fff;
   font-family: 'Source Sans Pro', Helvetica, sans-serif;
 `
