4a123b793c91a544f061b55764246e57ea201699
Caleb Sharp <epicsharp11@gmail.com>
2018-07-24 12:25:39 -0700

    Add styled-components gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: 'Shuswap Family Therapy',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-eslint'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-eslint',
+    'gatsby-plugin-styled-components',
+  ],
 }
