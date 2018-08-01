73e85a20127a3af47c290047765719d3edbc2d5b
Caleb Sharp <epicsharp11@gmail.com>
2018-08-01 16:21:04 -0700

    Add gatsby plugins to optimize images

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,17 @@ module.exports = {
     title: 'Shuswap Family Therapy',
   },
   plugins: [
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-eslint',
     'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      },
+    },
   ],
 }
