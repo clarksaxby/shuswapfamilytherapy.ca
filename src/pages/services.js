b9937d6515469cc497983ef650a4c08c77b5370e
Monika Piechatzek <piechatzek@hotmail.com>
2018-08-16 16:57:10 -0700

    Added basic Services layout on page

---
+++ src/pages/services.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components'
+import ContentPage from '../components/ContentPage'
+import Content, { Section } from '../components/Content'
+
+// grid-template-rows: 1fr 1fr;
+const ContentWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 10px 20px;
+  margin-bottom: 40px;
+  justify-items: center;
+
+  & > * {
+    text-align: center;
+  }
+
+  @media (max-width: 1000px) {
+    grid-template-columns: 1fr;
+    grid-gap: 10px 0px;
+  }
+  @media screen and (max-width: 565px) {
+    grid-gap: 10px 0px;
+  }
+`
+
+const Title = styled.h3`
+  margin: 0px;
+`
+// const Couples = styled.div``
+// const Family = styled.div``
+
+const Services = props => (
+  <Content>
+    <Section>
+      <ContentPage
+        title="Services"
+        subtitle="Services may be covered in part or in full by your extended health plan, please inquire of your specific plan if counselling sessions by a Registered Clinical Counsellor (RCC) are eligible for coverage."
+        footer="Shuswap Couple & Family Therapy 2018"
+      >
+        <ContentWrapper {...props}>
+          <div>
+            <Title>Individuals</Title> 50 minute sessions 110
+          </div>
+          <div>
+            <Title>Couples</Title> 50 minute sessions 125
+          </div>
+          <div>
+            <Title>Family</Title> 50 minute sessions 125
+          </div>
+        </ContentWrapper>
+      </ContentPage>
+    </Section>
+  </Content>
+)
+
+export default Services
